2b2da323bd1d4b566b165636c068c548d80bbf37
Damian Sloane <damian@capacitorlabs.com.au>
2021-06-17 19:20:06 +1000

    Work in progress

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 resource "aws_ssm_parameter" "ssm_parameter" {
   count = var.ignore_value_changes == false ? 1 : 0
+    allowed_pattern = ""
+  description = ""
   name = var.name
   type = var.secure ? "SecureString": "String"
   value = var.value
@@ -9,6 +11,8 @@ resource "aws_ssm_parameter" "ssm_parameter" {
 
 resource "aws_ssm_parameter" "ssm_parameter_ignore_changes" {
   count = var.ignore_value_changes == true ? 1 : 0
+  allowed_pattern = ""
+  description = ""
   name = var.name
   type = var.secure ? "SecureString": "String"
   value = var.value
